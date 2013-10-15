6dd20840f833152387575ee829e5295825e74276
Boian Tzonev <boiantz@gmail.com>
2013-10-15 15:56:03 +0300

    Migrate to closure-templates. Move parenscript definition to a separate file. Init bootstrap.

--- server.lisp
+++ server.lisp
@@ -1,36 +1,13 @@
 (in-package :bukvonesis)
 
 (defparameter *httpd* nil)
- 
-(defmacro standard-page ((&key title) &body body)
-  `(with-html-output-to-string (*standard-output* nil :prologue t :indent t)
-     (:html 
-       (:head 
-	(:meta :http-equiv "Content-Type" 
-	       :content    "text/html;charset=utf-8")
-	(:title ,title)
-	(:script :type "text/javascript" :src "/bukvonesis.js"))
-       (:body 
-	,@body))))
-	
+
 (defun index.html ()
-  (standard-page
-    (:title "bukvonesis")
-    (:h1 "Select Font:")
-    (:input :type "file" :id "font-selector" :onchange (ps-inline (upload-file
-							    (@ (chain document (get-element-by-id "font-selector")) files 0))))
-    (:canvas :id "bukvonesis" :width "1000" :height "1000")))
+  (bukvonesis.html:index))
 
 (defun bukvonesis.js ()
-  (setf (content-type*) "text/javascript")
-  (ps (defun upload-file (file)
-	(let ((xhr (new (*x-m-l-http-request)))
-	      (form-data (new (*form-data))))
-	  
-	  (chain form-data (append "fontfile" file))
-	  (chain console (log file))
-	  (chain xhr (open "POST" "font-upload"))
-	  (chain xhr (send form-data))))))
+  (with-open-file (out (merge-pathnames (make-pathname :name "bukvonesis" :type "js" :directory '(:relative "js")) bukvonesis-config:*base-directory*) :direction :output :if-exists :supersede :if-does-not-exist :create)
+    (princ (ps:ps-compile-file (merge-pathnames bukvonesis-config:*base-directory* "bukvonesis.paren")) out)))
   
 (defun font-upload ()
   (destructuring-bind (path name content-type) (post-parameter "fontfile")
@@ -41,10 +18,17 @@
   (setf *show-lisp-errors-p* t
 	*show-lisp-backtraces-p* t
 	*break-on-signals* nil)
-  
+
+  ;;compile js and html templates
+  (bukvonesis.js)
+  (closure-template:compile-template :common-lisp-backend (merge-pathnames "bukvonesis.soy" bukvonesis-config:*base-directory*))
+
+  ;;routes  
   (setf *dispatch-table* 
 	(list 'dispatch-easy-handlers 
-	      (create-prefix-dispatcher "/bukvonesis.js" 'bukvonesis.js)
+	      (create-folder-dispatcher-and-handler "/js/" (cl-fad:merge-pathnames-as-directory bukvonesis-config:*base-directory* "js/"))
+	      (create-folder-dispatcher-and-handler "/css/" (cl-fad:merge-pathnames-as-directory bukvonesis-config:*base-directory* "css/"))
+	      (create-folder-dispatcher-and-handler "/fonts/" (cl-fad:merge-pathnames-as-directory bukvonesis-config:*base-directory* "font/"))
 	      (create-prefix-dispatcher "/font-upload" 'font-upload)
 	      (create-prefix-dispatcher "/" 'index.html)))
 
