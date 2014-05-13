cfcaa61367b7b543695cf36d3b9c260461a34e46
Boian Tzonev <boiantz@gmail.com>
2014-05-13 07:52:37 +0300

    Setup tween js.

--- js/animate.js
+++ js/animate.js
@@ -1,27 +1,40 @@
-var paper;
-var bukvonesisFontName = 'Devonshire';
-var fontSize = 300;
+$(function() {
+    var canvas = document.getElementById('bukvonesis');
+    paper.setup(canvas);
 
-function drawLetter(letter, xPos) {
-    paper.text(
-	xPos,
-	paper.height / 2,
-	letter).attr({ 'font-family' : bukvonesisFontName,
-                       'font-size' : fontSize,
-                       'fill' : 'black',
-                       'text_anchor' : 'start'
-                     });
-};
+    var paths = [];
+    var offset = 0;
 
-function animate() {
-    var xPos = paper.canvas.offsetLeft + 100;
-    $.each(devonshire, function( key, value ) {
-	drawLetter(key, xPos);
-	xPos += 100;
-    });
-}
+    //$.each(devonshire, function(key, trials) {
+	var key = 'b';
+	paths[key] = [];
+	var trials = devonshire[key];
+	//$.each(trials, function(trial, value) {
 
-$(function(){
-    paper = new Raphael("bukvonesis", 1140, 715);
-    animate();
+	    var trial = trials.length-1;
+	    var value = trials[trial];
+	    paths[key][trial] = [];
+
+	    $.each(value, function(index, coords) {
+		var path = new paper.Path();
+		paths[key][trial].push(path);
+		path.strokeColor = new paper.Color(1/(index+1), 0, 0);
+
+		if (coords.length == 6) {
+		    path.add(new paper.Point(coords[0], coords[1]));
+		    path.quadraticCurveTo(new paper.Point(coords[2], coords[3]),
+					  new paper.Point(coords[4], coords[5]));
+		} else {
+		    path.add(new paper.Point(coords[0], coords[1]),
+			     new paper.Point(coords[2], coords[3]));
+		}
+		path.position.x += offset;
+	    });
+
+	//});
+	offset += 300;
+    //});
+
+    paper.view.draw();
+    console.log(paths);
 });
